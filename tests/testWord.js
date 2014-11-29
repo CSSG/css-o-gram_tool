2b793ba66e73683edb84b4986081efb42b99f28f
Dmitry Makhnev <dmitry.ivshin@gmail.com>
2014-11-29 17:26:54 +0300

    move all to /old dir add basis testing

---
+++ tests/testWord.js
@@ -0,0 +1,15 @@
+describe('Tests tests work', function () {
+
+
+    it('tests start', function () {
+        expect(true).toBe(true);
+    });
+
+    it('async tests support', function (done) {
+        setTimeout(function () {
+            expect(true).toBe(true);
+            done();
+        }, 100);
+    });
+
+});
